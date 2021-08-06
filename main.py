c1b16d838fd771898c954a1a3f27f0fd06068f45
Dan Ewing <legodaniele@gmail.com>
2021-08-06 02:46:09 +0000

    Basic working version, goes until it hits 1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from time import sleep
+ogNum = int(input("Number: "))
+num = ogNum
+
+while True:
+  if num % 2 == 0:
+    num = num / 2
+  
+  else:
+    num = ((3*num)+1)
+  
+  num = int(num)
+  print(str(num) + ": " + ("*" * num))
+  sleep(.1)
+  if num == 1:
+    print("Completed")
+    break
